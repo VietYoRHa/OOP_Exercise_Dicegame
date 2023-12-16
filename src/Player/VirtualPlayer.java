5ad0f777b34f64fcad177799414ecf17e7825ede
VietYoRHa <thaiviet04@gmail.com>
2023-12-17 00:12:34 +0700

    Add Console class and fix some issues

--- src/Player/VirtualPlayer.java
+++ src/Player/VirtualPlayer.java
@@ -2,14 +2,16 @@ package Player;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class VirtualPlayer extends Player{
     private String failMessage;
-    private static int seqVPlayer = 0;
+    public static int seqVPlayer = 0;
     private static List<String> lstFailMessage = Arrays.asList("Error 404: Success not found", "Mission: Failure", "Oops", "Nooooo");
+    private Random random = new Random();
 
     public VirtualPlayer() {
-        this.failMessage = lstFailMessage.get(seqVPlayer);
+        this.failMessage = lstFailMessage.get(random.nextInt(lstFailMessage.size()));
         seqVPlayer++;
         String vPlayerName = "VirtualPlayer" + seqVPlayer;
         this.setName(vPlayerName);
